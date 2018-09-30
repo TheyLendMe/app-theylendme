cb877ed871e4cb0247c610112abe0742782d8160
hugoramonpascual@gmail.com <hugoramonpascual@gmail.com>
2018-09-30 19:25:13 +0200

    Primera fase de objetos, (metodos y campos principales)

---
+++ flutter_app/lib/Objects/entity.dart
@@ -0,0 +1,89 @@
+abstract class Entity{
+  final String _idEntity;
+  final EntityType _type;
+  String _name,_desc,_img;
+  
+
+
+  Entity(this._type,this._idEntity,this._name,{String desc,String img}){
+
+  }
+
+
+  ///Getters and settes
+  String get idEntity => _idEntity;
+  String get name => _name;
+  String get desc => desc;
+
+  EntityType get type => _type;
+
+
+  set name(name) => _name = name;
+  set desc(desc) => _desc = desc;
+
+
+  ///Other functions
+  ///
+  
+
+  ///Add an object to a group or to a user.
+  void addObject();
+
+  void delObject();
+
+
+
+}
+
+
+
+class User extends Entity{
+  User(String idEntity, String name) : super(EntityType.USER, idEntity, name);
+
+  @override
+  void addObject() {
+    // TODO: implement addObject
+  }
+
+  @override
+  void delObject() {
+    // TODO: implement delObject
+  }
+
+
+
+
+}
+
+class Group extends Entity{
+  Group(EntityType type, String idEntity, String name) : super(EntityType.GROUP, idEntity, name);
+
+  @override
+  void addObject() {
+    // TODO: implement addObject
+  }
+
+  @override
+  void delObject() {
+    // TODO: implement delObject
+  }
+
+
+
+  void addUser(){
+
+  }
+
+  void delUser({User u}){
+  
+  }
+
+  
+
+}
+
+
+
+  enum EntityType {
+    USER, GROUP, Default
+  }
---
+++ flutter_app/lib/Objects/obj.dart
@@ -0,0 +1,161 @@
+
+import 'entity.dart';
+
+abstract class Obj{
+  final ObjType _type;
+  final String _idObject;
+  final Entity owner;
+
+  ///TODO pensar mejor el tema de las imagenes. Deberia de ser una lista???
+  String _name, _desc, _image;
+  // Group groupBorrowed;
+  // User userBorrowed;
+
+
+
+  ///Constructor
+  Obj(this._type,this._idObject,this.owner,String name, String desc, {String image =""}){
+    this._name = name;
+    this._desc = desc;
+    this._image = image;
+  }
+
+
+
+///Abstract Methods 
+  void lend(Entity e);
+
+  void request();
+
+  ///Devolver--> no me deja poner return D:
+  void bringBack();
+
+  void claim();
+
+  void setObjectInfo({String name, String desc});
+
+  void delObject({User user});
+
+///Getters and setters methods
+
+  String get name => _name;
+  String get desc => _desc;
+  String get image => _image;
+
+  String get idObject => _idObject;
+  ObjType get type => _type;
+  
+
+
+
+  set name(String name) => this._name = name;
+  set desc(String desc) => this._desc = desc;
+
+  ///TODO pensar mejor este tema
+  set image(String image) => this._name = image;
+
+
+}
+
+class UserObject extends Obj{
+
+
+  ///Constructor
+  UserObject(String idObject, Entity owner, String name, String desc) : super(ObjType.USER_OBJECT, idObject, owner, name, desc){
+
+
+
+  }
+
+  @override
+  void lend(Entity e) {
+    // TODO: implement lend
+  }
+
+  @override
+  void request() {
+    // TODO: implement request
+  }
+
+  @override
+  void bringBack() {
+    // TODO: implement bringBack
+  }
+
+  @override
+  void claim() {
+    // TODO: implement claim
+  }
+
+  @override
+  void setObjectInfo({String name, String desc}) {
+    // TODO: implement setObjectInfo
+  }
+
+  @override
+  void delObject({User user}) {
+    // TODO: implement delObject
+  }
+
+
+ 
+
+
+
+}
+
+
+
+class GroupObject extends Obj{
+
+
+  ///Constructor
+  GroupObject(String idObject, Entity owner, String name, String desc) : super(ObjType.GROUP_OBJECT, idObject, owner, name, desc){
+  }
+
+
+  @override
+  void lend(Entity e) {
+    // TODO: implement lend
+  }
+
+  @override
+  void request() {
+    // TODO: implement request
+  }
+
+  @override
+  void bringBack() {
+    // TODO: implement bringBack
+  }
+
+  @override
+  void claim() {
+    // TODO: implement claim
+  }
+
+  @override
+  void setObjectInfo({String name, String desc}) {
+    // TODO: implement setObjectInfo
+  }
+
+  
+  @override
+  void delObject({User user} ) {
+    // TODO: implement delObject
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+  enum ObjType {
+    USER_OBJECT, GROUP_OBJECT
+  }
