54ceb63addc07e617a24afb2653795680a3129dc
jartigag <jartigag@protonmail.com>
2018-10-11 11:51:09 +0200

    TextFormFields in SettingsPage

--- TheyLendMe/lib/pages/settings.dart
+++ TheyLendMe/lib/pages/settings.dart
@@ -1,45 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:validate/validate.dart'; //TODO: use validation logic
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    // WIP: implement build
-    return Scaffold (
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) =>
-            EntryItem(fields[index]),
-        itemCount: fields.length,
+    //TODO: secciones: Perfil, Seguridad (contraseña)
+    return Scaffold(
+      body: Container(
+        padding: new EdgeInsets.all(20.0),
+        child: new Form(
+          //key: this._formKey,
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) =>
+                EntryItem(fields[index]),
+            itemCount: fields.length,
+          )
+        )
       )
     );
   }
 }
 
 // Displays one Entry.
-class EntryItem extends StatelessWidget {
+class EntryItem extends StatefulWidget {
   const EntryItem(this.entry);
-
   final Entry entry;
 
-  Widget _buildTiles(Entry root) {
-    return ListTile(title: Text(root.title));
-  }
+  @override
+  State<StatefulWidget> createState() => new _EntryItemState();
+}
 
+class _EntryItemState extends State<EntryItem> {
   @override
   Widget build(BuildContext context) {
-    return _buildTiles(entry);
+    return TextFormField (
+      decoration: InputDecoration (
+        labelText: widget.entry.label,
+        hintText: widget.entry.hint
+      )
+    );
   }
 }
 
 final List<Entry> fields = <Entry> [
-  Entry('Nombre'),
-  Entry('Teléfono'),
-  Entry('Email'),
-  Entry('Descripción')
+  Entry('Nombre','Escribe tu nombre'),
+  Entry('Teléfono','Escribe tu número de teléfono'),
+  Entry('Email','tuemail@ejemplo.com'),
+  Entry('Descripción','Escribe algo sobre ti para que los demás usuarios te conozcan mejor.')
 ];
 
 class Entry {
-  Entry(this.title);
+  Entry(this.label, this.hint);
 
-  final String title;
-  //TODO: TextFields here
+  final String label;
+  final String hint;
+  //TODO: keyboardType?
 }
