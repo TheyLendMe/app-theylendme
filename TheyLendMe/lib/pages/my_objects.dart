5f59e10e707f50e17363175c22a7db93919827d6
calvo111979 <acalvoinfa10@gmail.com>
2018-10-18 18:02:40 +0200

    añadido crearObj

--- TheyLendMe/lib/pages/my_objects.dart
+++ TheyLendMe/lib/pages/my_objects.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'package:image_picker/image_picker.dart';
 
 class MyObjectsPage extends StatefulWidget {
     @override
@@ -18,10 +20,47 @@ class _MyObjectsPageState extends State<MyObjectsPage> {
       body: ListView.builder( //ListView de ejemplo:
         itemBuilder: (BuildContext context, int index) => ObjectItem(objects[index]),
         itemCount: objects.length
-      )
+      ),
+
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add,color: Colors.white),
+        onPressed: (){
+          showDialog(
+            context: this.context,
+            builder: (BuildContext context){
+              return SimpleDialog(
+                title: new Text('Crea tu objeto'),
+                children: <Widget>[
+                  new TextField(
+                    decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Nombre del objeto')),
+                  //new Image.network('https://wakyma.com/blog/wp-content/uploads/2017/10/Tipos-de-diarrea-en-gatos-y-su-tratamiento-770x460.'),
+                  new ImagePicker.pickImage(source: ImageSource.camera),
+                  new TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Descripción del objeto'
+                    )
+                  ),
+                  new FlatButton(
+                    child: new Text('Crear'),
+                  onPressed: (){
+                    //Aqui mandamos cosas a la base de datos
+                  Navigator.of(context).pushNamed('/MyObjectsPage');
+                  })               
+                ],
+                 
+              );
+            }
+          );
+          
+        },
+      ),    
     );
   }
 }
+
 
 // Displays one Object.
 class ObjectItem extends StatelessWidget {
