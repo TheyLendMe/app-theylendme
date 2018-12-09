5ba6bea25fa172b3d2582693b4fc576f13b2c66b
Hugo Ramon <hugoramonpascual@gmail.com>
2018-12-09 11:13:50 +0100

    #HotFIx groupdCode

--- TheyLendMe/lib/pages/private_code_dialog.dart
+++ TheyLendMe/lib/pages/private_code_dialog.dart
@@ -38,8 +38,9 @@ class _PrivateCodeDialogState extends State<PrivateCodeDialog> {
                         widget._group.getPrivateCode()
                         .then((code) {
                           _privateCode = code;
+                          launch('mailto:${snapshot.data.email}?subject=Código&body=Hola!%20Aquí%20tienes%20el%20código%20para%20unirte%20a%20nuestro%20grupo%20de%20TheyLendMe%20:%20$_privateCode');
                         });
-                        launch('mailto:${snapshot.data.email}?subject=Código&body=Hola!%20Aquí%20tienes%20el%20código%20para%20unirte%20a%20nuestro%20grupo%20de%20TheyLendMe%20:%20$_privateCode');
+                        
                       },
                       child: Icon( FontAwesomeIcons.at, color: Colors.black,  size: 50.0),
                     ): Text('') //esto nunca se mostrará porque siempre hay email
